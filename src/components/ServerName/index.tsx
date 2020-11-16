dce956b779fa08a4d99b3984fdc1a792320153d0
Wellington N <wellingto_07@hotmail.com>
2020-11-16 16:33:12 -0300

    componets servername

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container, Title, ExpandIcon } from "./styles";
+
+const SeverName: React.FC = () => {
+  return (
+    <Container>
+      <Title>Servidor do Rodz</Title>
+
+      <ExpandIcon />
+    </Container>
+  );
+};
+
+export default SeverName;
